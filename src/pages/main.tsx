33cdd2d566629edc8e2502b352e6ed2cc060761a
viadsten <stanulevich.vg@yandex.ru>
2023-07-23 17:08:04 +0300

    feat: добавялет компонент CityFilter

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,10 +1,11 @@
-import React, {ReactNode, useState} from 'react';
+import React, {ReactNode, useEffect, useState} from 'react';
 import OfferFilter from '../components/offer-filter/offer-filter';
 import OfferList from '../components/offer-list/offer-list';
 import Map from '../components/map/map';
 import {OfferPreviewProps} from '../types/offer-props';
 import Header from '../components/header/header';
 import {Cities} from '../const';
+import CityFilter from '../components/city-filter/city-filter';
 
 interface MainProps {
   offerList: OfferPreviewProps[];
@@ -13,7 +14,6 @@ interface MainProps {
 function Main({offerList}:MainProps):ReactNode {
   const [currentCity, setCurrentCity] = useState<typeof Cities[number]>(Cities[0]);
   const [activeOffer, setActiveOffer] = useState<OfferPreviewProps | null>(null);
-  window.currentCity = currentCity;
 
   return (
     <div className='page page--gray page--main'>
@@ -21,27 +21,13 @@ function Main({offerList}:MainProps):ReactNode {
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              {
-                Cities.map((city) =>
-                  (
-                    <li className="locations__item" key={city}>
-                      <a className="locations__item-link tabs__item" href="#" onClick={() => setCurrentCity(city)}>
-                        <span>{city}</span>
-                      </a>
-                    </li>
-                  )
-                )
-              }
-            </ul>
-          </section>
+          <CityFilter currentCity={currentCity} setCurrentCity={setCurrentCity} />
         </div>
         <div className="cities">
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{offerList.length} places to stay in Amsterdam</b>
+              <b className="places__found">{offerList.length} places to stay in {currentCity}</b>
               <OfferFilter />
               <div className="cities__places-list places__list tabs__content">
                 <OfferList
