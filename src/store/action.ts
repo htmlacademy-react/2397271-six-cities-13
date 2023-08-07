a30e3ee97599d5708fc5626e96cc22140c297fe6
viadsten <stanulevich.vg@yandex.ru>
2023-08-07 21:41:41 +0300

    fix: меняет названия экшенов офера

--- src/store/action.ts
+++ src/store/action.ts
@@ -2,6 +2,6 @@ import {createAction} from '@reduxjs/toolkit';
 import {CityNameType} from '../types/location';
 import {OfferSortList} from '../const';
 
-export const changeCity = createAction<{ city: CityNameType }>('changeCity');
-export const changeOffersSort = createAction<{ sort: OfferSortList }>('changeSort');
+export const changeCity = createAction<{ city: CityNameType }>('offers/changeCity');
+export const changeOffersSort = createAction<{ sort: OfferSortList }>('offers/changeSort');
 
