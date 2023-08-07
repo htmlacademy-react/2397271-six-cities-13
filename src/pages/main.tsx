f3e3ac0f9f56fee9ef8938fad87a69c741d8ca1b
viadsten <stanulevich.vg@yandex.ru>
2023-08-07 21:57:26 +0300

    feat: добавляет проверку на пустую выдачу

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -12,6 +12,8 @@ import {selectOffersByCity, selectOffersBySortAndCity} from '../store/selectors/
 function Main():ReactNode {
   const [activeOffer, setActiveOffer] = useState<OfferPreviewType | null>(null);
   const currentCity: CityNameType = useAppSelector((state) => state.city);
+  const selectedOffers = useAppSelector(selectOffersByCity);
+  const selectedAndSortedOffers = useAppSelector(selectOffersBySortAndCity);
 
   return (
     <div className='page page--gray page--main'>
@@ -22,31 +24,46 @@ function Main():ReactNode {
           <CityFilter />
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{useAppSelector(selectOffersByCity).length} places to stay in {currentCity}</b>
-              <OfferFilter />
-              <div className="cities__places-list places__list tabs__content">
-                <OfferList
-                  offerList={useAppSelector(selectOffersBySortAndCity)}
-                  className='cities'
-                  handleMouseEnter={(offer:OfferPreviewType) => {
-                    setActiveOffer(offer);
-                  }}
-                />
+          {
+            !useAppSelector(selectOffersByCity).length
+              ?
+              <div className="cities__places-container cities__places-container--empty container">
+                <section className="cities__no-places">
+                  <div className="cities__status-wrapper tabs__content">
+                    <b className="cities__status">No places to stay available</b>
+                    <p className="cities__status-description">We could not find any property available at the moment in {currentCity}</p>
+                  </div>
+                </section>
+                <div className="cities__right-section"></div>
               </div>
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map">
-                <Map
-                  city={useAppSelector(selectOffersByCity)[0].city}
-                  offerList={useAppSelector(selectOffersByCity)}
-                  activeOffer={activeOffer}
-                />
-              </section>
-            </div>
-          </div>
+              :
+              <div className="cities__places-container container">
+                <section className="cities__places places">
+                  <h2 className="visually-hidden">Places</h2>
+                  <b className="places__found">{selectedOffers.length} places to stay in {currentCity}</b>
+                  <OfferFilter />
+                  <div className="cities__places-list places__list tabs__content">
+                    <OfferList
+                      offerList={selectedAndSortedOffers}
+                      className='cities'
+                      handleMouseEnter={(offer:OfferPreviewType) => {
+                        setActiveOffer(offer);
+                      }}
+                    />
+                  </div>
+                </section>
+                <div className="cities__right-section">
+                  <section className="cities__map map">
+                    <Map
+                      city={selectedAndSortedOffers[0].city}
+                      offerList={selectedOffers}
+                      activeOffer={activeOffer}
+                    />
+                  </section>
+                </div>
+              </div>
+          }
+
         </div>
       </main>
     </div>);
