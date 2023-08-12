1fea1d0a2b1553b377d9afd41d0d52736fda7667
viadsten <stanulevich.vg@yandex.ru>
2023-08-12 11:44:29 +0300

    feat: выполняет задание 7.2

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import Header from '../components/header/header';
+import {regexEmail, regexPassword} from '../helpers/validator';
+import {toast} from 'react-toastify';
+import {AppRoute, AuthorizationStatus, ValidateErrors} from '../const';
+import {store} from '../store';
+import {loginAction} from '../store/api-action';
+import {useAppSelector} from '../hooks';
+import {Navigate} from 'react-router-dom';
 
 function Login() {
+  const [formState, setFormState] = useState({email: '', password: ''});
+  const authorizationStatus: AuthorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  const handleFormSubmit = (event:FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!regexEmail.test(formState.email)) {
+      toast.error(ValidateErrors.IncorrectEmail);
+      return;
+    }
+    if (formState.password.length < 6) {
+      toast.error(ValidateErrors.ShortPassword);
+      return;
+    }
+
+    if (!regexPassword.test(formState.password)) {
+      toast.error(ValidateErrors.NoNumberPassword);
+      return;
+    }
+
+    store.dispatch(loginAction(formState));
+  };
+
+  if (authorizationStatus === AuthorizationStatus.Auth) {
+    return <Navigate to={AppRoute.root} />;
+  }
+
   return (
     <div className="page page--gray page--login">
       <Header />
@@ -9,14 +42,40 @@ function Login() {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form
+              className="login__form form"
+              method="post"
+              onSubmit={(event) => handleFormSubmit(event)}
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required="" />
+                <input
+                  className="login__input form__input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required=""
+                  onChange={
+                    (event) => setFormState(
+                      (prevState) => ({...prevState, email: event.target.value})
+                    )
+                  }
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required="" />
+                <input
+                  className="login__input form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required=""
+                  onChange={
+                    (event) => setFormState(
+                      (prevState) => ({...prevState, password: event.target.value})
+                    )
+                  }
+                />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
--- src/store/api-action.ts
+++ src/store/api-action.ts
@@ -1,9 +1,11 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AxiosInstance} from 'axios';
 import {AppDispatch, State} from '../types/root-state';
-import {APIRoute} from '../const';
+import {APIRoute, AuthorizationStatus} from '../const';
 import {OfferPreviewType} from '../types/offer';
-import {fetchOffers, setIsOffersLoading} from './action';
+import {fetchOffers, requireAuthorization, setIsOffersLoading, setUserData} from './action';
+import {dropToken, saveToken} from '../services/token';
+import {AuthData, UserData} from '../types/user';
 
 export const fetchOffersAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -17,4 +19,48 @@ export const fetchOffersAction = createAsyncThunk<void, undefined, {
     dispatch(setIsOffersLoading(false));
     dispatch(fetchOffers(data));
   }
+);
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      const {data} = await api.get<UserData>(APIRoute.Login);
+      dispatch(setUserData(data));
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
+  },
+);
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async ({email, password}, {dispatch, extra: api}) => {
+    const {data} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(data.token);
+    dispatch(setUserData(data));
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  },
+);
+
+export const logoutAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/logout',
+  async (_arg, {dispatch, extra: api}) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+  },
 );
