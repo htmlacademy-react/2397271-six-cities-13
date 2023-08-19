c5d0f196465568a4dcea964fd56144c966064566
viadsten <stanulevich.vg@yandex.ru>
2023-08-19 16:23:37 +0300

    fix: рефакторит сортировку

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import ReviewItem from '../review-item/review-item';
 import {ReviewType} from '../../types/offer';
 import {MAX_REVIEW_AMOUNT} from '../../const';
+import dayjs from 'dayjs';
 
 interface ReviewListProps {
   reviews: ReviewType[];
 }
 
 function ReviewList({reviews}:ReviewListProps) {
-  const sortedByDate = [...reviews].sort((a, b) => new Date(b.date) - new Date(a.date));
+  const sortedReviews = [...reviews]
+    .sort((a, b) => dayjs(b.date).diff(a.date))
+    .slice(0, MAX_REVIEW_AMOUNT);
 
   return (
-    <ul className="reviews__list">
+    <ul className="reviews__list" data-testid='review-list-container'>
       {
-        sortedByDate
-          .slice(0, MAX_REVIEW_AMOUNT)
-          .map((review) => <ReviewItem comment={review} key={review.id}/>)
+        sortedReviews.map((review) => <ReviewItem comment={review} key={review.id}/>)
       }
     </ul>
   );
