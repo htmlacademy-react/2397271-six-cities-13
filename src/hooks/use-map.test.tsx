afbe1d70b0d4c349bab3a535eed419c683fcc77a
viadsten <stanulevich.vg@yandex.ru>
2023-08-27 22:42:22 +0300

    fix: правит ошибки линтера

--- src/hooks/use-map.test.tsx
+++ src/hooks/use-map.test.tsx
@@ -1,19 +1,21 @@
-import { render, renderHook } from "@testing-library/react";
-import { useRef } from "react";
-import { makeFakeOffer } from "../utils/mocks/offer";
-import useMap from "./use-map";
+import { render, renderHook } from '@testing-library/react';
+import { useRef } from 'react';
+import { makeFakeOffer } from '../utils/mocks/offer';
+import useMap from './use-map';
 
 describe('Hook: useMap', () => {
-  const {result} = renderHook(() => useRef(null));
-  const mapRef = result.current;
+  const getMapRef = () => {
+    const {result} = renderHook(() => useRef(null));
+    return result.current;
+  };
+  const mapRef = getMapRef();
   const city = makeFakeOffer().city;
-  const MapEl = () => <div ref={result.current}></div>;
+  const MapEl = () => <div ref={mapRef}></div>;
 
   render(<MapEl/>);
 
   it('should return map instance', () => {
     const { result } = renderHook(() => useMap(mapRef, city));
-    console.log(result.current);
 
     expect(typeof result.current).toBe('object');
   });
